9238d0fa5ca2225622f636efeb1e9422fa8201cb
Yun-010606 <2944408214@qq.com>
2022-06-26 20:14:53 +0800

    完成头部自定义

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,9 +1,25 @@
 <template>
-  <div class="my-header"></div>
+  <div class="my-header" :style="{background, color}">{{title}}</div>
 </template>
 
 <script>
 export default {
+  // props 可以是一个数组 也可以是一个对象
+  // 对象： {props属性名： value}
+  // value 可以是个类型 也可以是个对象
+  // value 是个对象 {默认值default:'', 数据类型type:类型, 是否必须要required:true/false}
+  // default 和 required 写一个就行
+  props: {
+    title: String,
+    background: {
+      type: String,
+      default: 'skyblue'
+    },
+    color: {
+      type: String,
+      required: true
+    }
+  }
 }
 </script>
 
